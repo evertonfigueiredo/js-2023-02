72e1baf557b4438ddcb8962e2dd91b852f7ac7c0
Everton Figueiredo <j.everton.figueiredo@gmail.com>
2023-04-25 20:42:21 -0300

    While

--- index2.js
+++ index2.js
@@ -8,38 +8,65 @@
 //     alert("Você é menor de idade")
 // }
 
-let dia = prompt("Qual o dia da semana?")
+// let dia = prompt("Qual o dia da semana?")
 
-switch (dia) {
-    case "0":
-        alert("Hoje é Domingo")
-        break;
+// switch (dia) {
+//     case "0":
+//         alert("Hoje é Domingo")
+//         break;
 
-    case "1":
-        alert("Hoje é Segunda")
-        break;
+//     case "1":
+//         alert("Hoje é Segunda")
+//         break;
 
-    case "2":
-        alert("Hoje é Terça")
-        break;
+//     case "2":
+//         alert("Hoje é Terça")
+//         break;
 
-    case "3":
-        alert("Hoje é Quarta")
-        break;
+//     case "3":
+//         alert("Hoje é Quarta")
+//         break;
 
-    case "4":
-        alert("Hoje é Quinta")
-        break;
+//     case "4":
+//         alert("Hoje é Quinta")
+//         break;
 
-    case "5":
-        alert("Hoje é Sexta")
-        break;
+//     case "5":
+//         alert("Hoje é Sexta")
+//         break;
 
-    case "6":
-        alert("Hoje é Sábado")
-        break;
+//     case "6":
+//         alert("Hoje é Sábado")
+//         break;
 
-    default:
-        alert("Dia não existente!")
-        break;
+//     default:
+//         alert("Dia não existente!")
+//         break;
+// }
+
+// let nome1 = "Everton"
+// let nome2 = "Bea"
+// let nome3 = "Guilherme"
+// let nome4 = "Lazaro"
+
+let nome = ["Everton","Bea","Guilherme"]
+
+// console.log(nome);
+
+// for (let index = 0; index < nome.length; index++) {
+//     console.log(nome[index]);
+// }
+
+let controler = true
+
+while (controler) {
+
+    let idade = prompt("Qual sua idade?")
+
+    if (idade >= 18) {
+        console.log("Você é maior de idade");
+        controler = false
+    }else{
+        console.log("Informe uma idade maior que 17");
+    }
 }
