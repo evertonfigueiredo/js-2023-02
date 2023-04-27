bccc2e7b80abb6ccfe15dc4901c56009b88ba1b8
Everton Figueiredo <j.everton.figueiredo@gmail.com>
2023-04-27 20:35:35 -0300

    Atividade de Array

---
+++ index3.js
@@ -0,0 +1,79 @@
+// let pessoas = ["Everton", "Jessica", "Anny", "Elias"]
+
+// console.log(pessoas);
+
+// for (let index = 0; index < pessoas.length; index++) {
+// console.log(`${index} - ${pessoas[index]}`);
+// }
+
+
+// console.log("---------------------------");
+
+// pessoas.push("Denis")
+
+// for (let index = 0; index < pessoas.length; index++) {
+// console.log(`${index} - ${pessoas[index]}`);
+// }
+
+// console.log("---------------------------");
+
+// pessoas.pop()
+
+// for (let index = 0; index < pessoas.length; index++) {
+// console.log(`${index} - ${pessoas[index]}`);
+// }
+
+// console.log("---------------------------");
+
+// pessoas.unshift("Natalia")
+
+// for (let index = 0; index < pessoas.length; index++) {
+// console.log(`${index} - ${pessoas[index]}`);
+// }
+
+// console.log("---------------------------");
+
+// pessoas.shift()
+
+// for (let index = 0; index < pessoas.length; index++) {
+//     console.log(`${index} - ${pessoas[index]}`);
+// }
+
+// console.log("---------------------------");
+
+// Mostra o intervalo entre o primeiro parametro e o segundo parametro
+// console.log(pessoas.slice(1,3));
+
+// console.log(pessoas);
+
+// console.log("---------------------------");
+
+// Modifica o array
+// pessoas.splice(1,1)
+// pessoas.splice(2,1)
+
+// console.log(pessoas);
+
+// let nome = "Anny"
+
+// let resultado = pessoas.indexOf(nome)
+
+// console.log(resultado);
+
+// if (resultado === -1) {
+//     console.log("Pessoa não encontrada");
+// }else{
+//     pessoas.splice(resultado, 1)
+// }
+
+// console.log(pessoas);
+
+let produtos = []
+
+for (let index = 0; index < 5; index++) {
+    let nome = prompt("Qual o produto?")
+
+    produtos.push(nome)
+}
+
+console.log(produtos);
