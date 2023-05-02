a4afbf47434aed2b0b43e708a2543e5543847ec2
Everton Figueiredo <j.everton.figueiredo@gmail.com>
2023-05-02 20:21:00 -0300

    Parte 1 da aula de funções

---
+++ index4.js
@@ -0,0 +1,32 @@
+// function mostrarDados(nome, idade) {
+//     if (nome !== undefined) {
+//         console.log(`Seu nome é: ${nome}`);
+//     } else {
+//         console.log(`Seu nome não é valido.`);
+//         return false
+//     }
+    
+//     if (idade !== undefined) {
+//         console.log(`Sua idade é: ${idade}`);
+//     } else {
+//         console.log(`Sua idade não é valida.`);
+//         return false
+//     }
+
+//     return true
+    
+// }
+
+// function mostrarInfo(num1, num2) {
+//     console.log(`A soma dos dois numeros é: ${num1 + num2}`);
+ 
+// }
+
+// function pegarDados(funcao) {
+//     let nome = prompt("Qual o seu nome: ")
+//     let idade = prompt("Qual a sua idade: ")
+
+//     funcao(nome,idade)
+// }
+
+// pegarDados(mostrarDados)
